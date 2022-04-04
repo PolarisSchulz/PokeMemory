16f212193949da48a2fb76c411bbdcf9a7fbb183
Abigail Peña <97539104+PolarisSchulz@users.noreply.github.com>
2022-04-03 23:56:24 -0500

    git cambios en boton reset y tiempos

--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
 // importar lo que esta en app (esto ya estaba en el documento)
-
 import App from "./components/App.js";
 document.getElementById("root").appendChild(App());
 
-// importar pokemon.js
+// importar la data pokemon.js
 import pokemon from "./data/pokemon/pokemon.js";
-
-// importar la data
-import getDataPokemon from "./data/pokemon/pokemon.js";
-console.log(getDataPokemon);
 
 // dos arrays de pokemon de los datos para hacer el match
 const arrayPokemonOriginal = pokemon.items;
@@ -21,48 +16,15 @@ let dataPokemonOriginalAndCopy = arrayPokemonOriginal.concat(arrayPokemonCopy);
 // console.log(dataPokemonOriginalAndCopy);
 
 // Acá se revuelve, sort permite ordenar los elementos de un arreglo de acuerdo al resultado de una funcion que en este caso será su parametro
-
 //Math.random los va a desaordenar y ordenarlos de manera diferente en cada partida
-
 const randomPokemon = dataPokemonOriginalAndCopy.sort(() => {
   return Math.random() - 0.5;
 });
 
-// let randomnize = "";
-// for (let index = 0; index < 18; index++) {
-//   const print = document.getElementById("pokeMatchCards");
-//   randomnize += `<div class="container">
-//       <div class="memoryMatch">
-//       <div class="frontCardPokemon">
-//       <img src="../img/pokebolatarjeta-08.png">
-//       </div>
-//       <section class="backCardPokemon">
-//       <img src="${randomPokemon[index].image}" alt="" <h3>${randomPokemon[index].id}</h3></section>
-//       </div>
-//     </div>`;
-
-//   print.innerHTML = randomnize;
-// }
-
-// let randomnize = "";
-// for (let index = 0; index < 18; index++) {
-//   const print = document.getElementById("pokeMatchCards");
-//   randomnize += `
-//       <div class="dashboardCards">
-//       <div class="frontCardPokemon">
-//       <img src="../img/pokebolatarjeta-08.png">
-//       </div>
-//       <section class="backCardPokemon">
-//       <img src="${randomPokemon[index].image}" alt="" <h3>${randomPokemon[index].id}</h3></section>
-//       </div>
-//     </div>`;
-
-//   print.innerHTML = randomnize;
-// }
-
+// Para imprimir la data en el DOM
 // pokeMatchCards-seccion den la linea 25 del HTML donde se colocan las cartas de pokemon
-// dashboardCards-Cartas de pokemon donde hacemos que tengan movimiento
-// cardsPokemon-Cartas de pokemon, donde hacemos que esten quietas
+// dashboardCards-Cartas de pokemon donde vamos a hacer que tengan movimiento
+// cardsPokemon-Cartas de pokemon, donde hacemos que se pegue el lado del frente con el lado de atras y le diga al codigo que es un solo objeto
 // backCardPokemon-Donde estan los pokemones
 // frontCardPokemon-Donde esta la imagen de la pokebola
 let randomnize = "";
@@ -72,7 +34,7 @@ for (let index = 0; index < 18; index++) {
       <div class="dashboardCards">
       <div class="cardsPokemon">
       <div class="cardCommon backCardPokemon">
-      <img src="${randomPokemon[index].image}" alt="" <h3>${randomPokemon[index].id}</h3>
+      <img src="${randomPokemon[index].image}" <h3>${randomPokemon[index].id}</h3>
       </div>
       <div class="cardCommon frontCardPokemon">
       <img src="../img/pokebolatarjeta-08.png">
@@ -83,9 +45,26 @@ for (let index = 0; index < 18; index++) {
   print.innerHTML = randomnize;
 }
 
-let cards = document.getElementsByClassName("cardsPokemon");
-for (const card of cards) {
+//variable selectCards, es igual a manipular el elemento con la clase "cardsPokemon", linea 41 de main.js
+// el ciclo for itera la nueva constante creada que llamaremos card dentro de selectCards, donde al ejecutar el evento clic (seleccionar la carta), la funcion anonima insertada, hará que la constante card ejecute la propiedad elemento class list con el metodo toggle que es un token booleano, se enlace con is-flipped a la linea 73 de style.css y genere el giro de 180 grados
+let selectCards = document.getElementsByClassName("cardsPokemon");
+for (const card of selectCards) {
   card.addEventListener("click", function () {
     card.classList.toggle("is-flipped");
   });
+}
+
+// Resetear juego
+// Sort arregla los elementos de un array dependiendo del criterio que nosotros querramos aplicar
+// to string, convierte un objeto en un string, por ejemplo un numero dentro de un array
+function resetGame() {
+  card.sort(function () {
+    return Math.random() - 0.5;
+  });
+
+  for (var index = 0; index < 18; index++) {
+    let card = card[index].id;
+    let dato = document.getElementById(index.toString());
+    dato.dataset.valor = carta;
+  }
 }
