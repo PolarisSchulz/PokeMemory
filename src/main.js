b56a68467c338c5f4b3373be0139cdc0f7b21216
Abigail Peña <97539104+PolarisSchulz@users.noreply.github.com>
2022-04-04 19:10:14 -0600

    "cambios en la estructura de  el match de cartas"

--- src/main.js
+++ src/main.js
@@ -35,10 +35,10 @@ for (let index = 0; index < 18; index++) {
   randomnize += `
     <div class="dashboardCards">
       <div class="cardsPokemon">
-        <div id="${randomPokemon[index].id}" class="cardCommon backCardPokemon">
+        <div id="${index}" class="cardCommon backCardPokemon ${randomPokemon[index].id}" >
           <img src="${randomPokemon[index].image}" <h3>${randomPokemon[index].id}</h3>
         </div>
-        <div class="cardCommon frontCardPokemon">
+        <div class="cardCommon frontCardPokemon ${randomPokemon[index].id}">
           <img src="../img/pokebolatarjeta-08.png">
         </div>
       </div>
@@ -49,48 +49,84 @@ for (let index = 0; index < 18; index++) {
 
 //variable selectCards, es igual a manipular el elemento con la clase "cardsPokemon", linea 41 de main.js
 // el ciclo for itera la nueva constante creada que llamaremos card dentro de selectCards, donde al ejecutar el evento clic (seleccionar la carta), la funcion anonima insertada, hará que la constante card ejecute la propiedad elemento class list con el metodo toggle que es un token booleano, se enlace con is-flipped a la linea 73 de style.css y genere el giro de 180 grados
+
+// flippedCards solo da informacion de cuantas tarjetas se estan usando
+let flippedCards = null;
+let pokeId1 = null;
+let pokeId2 = null;
+let flips = 0;
+let firstPokemon = null;
+let secondPokemon = null;
+let score = 0;
+
+// variable para apuntar en el HTML el lugar que se esta ocupando los flips y score linea 36 y 38
+let showFlips = document.getElementById("flips");
+let showScore = document.getElementById("score");
+
 let selectCards = document.getElementsByClassName("cardsPokemon");
 for (const card of selectCards) {
   card.addEventListener("click", function () {
     // list almacena todos los elementos con la clase is flipped
-    let list = document.getElementsByClassName("is-flipped");
-    if (list.length == 0) {
+    flippedCards++;
+    if (flippedCards == 1) {
       // el elemento if pregunta cuantos elementos hay en esa lista
       card.classList.toggle("is-flipped");
-      pokeId = card.firstElementChild.id;
-      // poke id almacena el id de la primera carta que se da vuelta
+      pokeId1 = document.getElementById(card.firstElementChild.id);
+      firstPokemon = card.firstElementChild.innerText;
+      pokeId1.disabled = true;
+      // pokeId1 almacena el id de la primera carta que se da vuelta
     } else {
-      if (pokeId == card.firstElementChild.id) {
+      if (flippedCards == 2) {
         // el siguiente if pregunta si el ID almacenado es igual al ID de la carta actual
         card.classList.toggle("is-flipped");
-        let element = document.getElementById(pokeId);
-        element.classList.add("fakeFlipped");
-        // intentando hacer los cambios de clase de is flipped a fake flipped
-        document.getElementById(pokeId).classList.add("fakeFlipped");
-        document.getElementById(pokeId).classList.remove("is-flipped");
+        pokeId2 = document.getElementById(card.firstElementChild.id);
+        secondPokemon = card.firstElementChild.innerText;
+        pokeId2.disabled = true;
+        flips++;
+        showFlips.innerHTML = `Movimientos: ${flips}`;
 
-        element.classList.remove("is-flipped");
-      } else {
-        // aqui la carta activa es distinta a la carta almacenada
-        // esta seccion intenta regresar a la carta para que se vea la pokebola
-        card.classList.toggle("is-flipped");
-        document.getElementById(pokeId).classList.toggle("is-flipped");
+        if (firstPokemon === secondPokemon) {
+          flippedCards = 0;
+
+          score++;
+          showScore.innerHTML = `Aciertos: ${score}`;
+        } else {
+          // pokeId1.classList.toggle("is-flipped");
+          // pokeId2.classList.toggle("is-flipped");
+
+          pokeId1.disabled = false;
+          pokeId2.disabled = false;
+          eventFire(pokeId1, "click");
+          eventFire(pokeId2, "click");
+
+          flippedCards = 0;
+        }
       }
     }
   });
 }
 
+function eventFire(el, etype) {
+  if (el.fireEvent) {
+    el.fireEvent("on" + etype);
+  } else {
+    var evObj = document.createEvent("Events");
+    evObj.initEvent(etype, true, false);
+    el.dispatchEvent(evObj);
+  }
+}
+
 // Resetear juego
 // Sort arregla los elementos de un array dependiendo del criterio que nosotros querramos aplicar
 // to string, convierte un objeto en un string, por ejemplo un numero dentro de un array
-// function resetGame() {
-//   card.sort(function () {
-//     return Math.random() - 0.5;
-//   });
+function resetGame() {
+  card.sort(function () {
+    return Math.random() - 0.5;
+  });
 
-//   for (var index = 0; index < 18; index++) {
-//     let card = card[index].id;
-//     let dato = document.getElementById(index.toString());
-//     dato.dataset.valor = carta;
-//   }
-// }
+  for (var index = 0; index < 18; index++) {
+    let card = card[index].id;
+    let dato = document.getElementById(index.toString());
+    dato.dataset.valor = carta;
+  }
+}
